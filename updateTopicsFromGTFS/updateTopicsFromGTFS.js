e443be03161a4f8540b5705ad9a32d3128f54dd7
Daniel Everett <daniel.everett@gmail.com>
2019-10-01 08:13:51 +1000

    Begin topic update changes

--- updateTopicsFromGTFS/updateTopicsFromGTFS.js
+++ updateTopicsFromGTFS/updateTopicsFromGTFS.js
@@ -6,18 +6,16 @@ const parser = require('csv-parser');
 exports.handler = async (event) => {
 
 	const db = new Map([
-		[ 'routes.txt', { transform : (data) => [data.route_id, data.route_desc], filter: (data) => true }],
+		[ 'routes.txt', { transform : (data) => ['route_'+data.route_id, data.route_desc], filter: (data) => true }],
 		[ 'stops.txt', {
-			transform : (data) => [data.stop_id, data.stop_name],
+			transform : (data) => ['stop_'+data.stop_id, data.stop_name],
 			filter : (data) => !data.stop_name.includes('Platform')  // exclude Platform stops, they add massive qty and never seem to get used anyway
 		} ],
 		[ 'trips.txt', {
-			transform : (data) => [data.trip_id.replace(/\./g,'_'), data.trip_headsign], // remove invalid SNS topic characters
+			transform : (data) => ['trip_'+data.trip_id.replace(/\./g,'_'), data.trip_headsign], // remove invalid SNS topic characters
 			filter : (data) => true
 		} ]
 	]);
-
-	const updateDate = new Date();
 
 	return Promise.all(event.Records.map(r => {
 		if (db.has(r.s3.object.key)) {
@@ -33,11 +31,9 @@ exports.handler = async (event) => {
 				.on('data', data => {
 					if (filter(data)) {
 						const [name, title] = transform(data);
-						console.log(title);
 						topics.push(sns.createTopic( {
 							Name : name,
-							Attributes : { DisplayName : title	},
-							Tags : [ { Key : 'Updated', Value : updateDate.valueOf().toString() } ]
+							Attributes : { DisplayName : title	}
 						}).promise());
 					}
 				})
@@ -53,26 +49,24 @@ exports.handler = async (event) => {
 	}))
 }
 
-function topicCleanup(newtopics, sns) {
-
-	const delPromises = [];
-
-	const cb = (err, data) => {
-		if (!err) {
-			if (data.NextToken) { 
-				sns.listTopics({ NextToken : data.NextToken }, cb);
-			} else {
-				//TODO warn
-			}
-
-			delPromises.push(
-				data.Topics
-				.map(topic => sns.deleteTopic(topic).promise())
-			)
-		}
+async function getTopics(sns, nextToken) {
+	const topics = await sns.listTopics({NextToken : nextToken}).promise();
+	if (topics.NextToken) {
+		return topics.Topics.concat(await getTopics(sns, topics.NextToken))
+	} else {
+		return topics.Topics
 	}
-
-	sns.listTopics({}, cb);
-	return Promise.all(delPromises);
 }
 
+async function topicCleanup(newtopics, sns) {
+
+	const topics = await getTopics(sns);
+	return topics.map(topic => sns.deleteTopic(topic).promise());
+}
+
+//topicCleanup('', sns).then(console.log)
+const ev = { Records : [ { s3 :  {
+	object : { key : 'trips.txt' },
+	bucket : { name : 'tfnsw-gtfs' }
+} } ] }
+exports.handler(ev).then(console.log)
